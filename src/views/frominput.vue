5195f34254c2e6dda49c506942438b9a1c569b4f
janyamild <32623881+mildjny@users.noreply.github.com>
2020-05-09 13:36:10 +0700

    from test

---
+++ src/views/frominput.vue
@@ -0,0 +1,45 @@
+<template>
+ <v-container>
+    <h2>test</h2>
+    <div id="comment">
+    <v-col cols="12" sm="6">
+      <div>
+        <!--card-->
+        <v-card max-width="344">
+          <v-list-item three-line>
+            <v-list-item-content>
+              <v-list-item-title class="headline mb-1">hello</v-list-item-title>
+              <v-list-item-subtitle>{{name}}</v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
+        </v-card>
+      </div>
+
+      <br />
+      <hr />
+    </v-col>
+    <!--form-->
+    <v-col cols="12" sm="6">
+      <div class="form-group">
+        <div>
+          <v-textarea label="Comment"></v-textarea>
+        </div>
+        <v-text-field label="name" single-line solo></v-text-field>
+        <div class="my-2">
+          <v-btn color="warning" dark>comment</v-btn>
+        </div>
+      </div>
+    </v-col>
+    </div>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    name: 'frominput',
+
+    data: () => ({
+ //
+  })
+  }
+</script>
