54deae8c1ecd276e0afa5223711b11274e49877f
janyamild <32623881+mildjny@users.noreply.github.com>
2020-05-12 03:58:19 +0700

    home

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,43 +2,49 @@
   <v-container>
     <v-layout row>
       <v-flex xs12 md8>
-          <v-col cols="12">
-            <v-img :src="require('../assets/pic1.jpg')" max-height="200" />
-          </v-col>
-          </v-flex>
-<v-flex xs12 md8>
-          <v-col v-for="(item, i) in items" :key="i" cols="12" xs12 md8>
-            <v-card  >
-              <div class="d-flex flex-no-wrap justify-space-between">
-                <v-avatar class="ma-4" size="125" tile>
-                  <v-img :src="require('../assets/golflogo.jpg')" width="300" />
-                </v-avatar>
-                <div>
-                  <v-card-title
-                    class="headline"
-                    v-text="item.title"
-                  ></v-card-title>
-
-                  <v-card-subtitle
-                    v-text="item.artist"
-                  ></v-card-subtitle>
-                </div>
-              </div>
-            </v-card>
-          </v-col>
-
-        
+        <v-col cols="12">
+          <v-img :src="require('../assets/pic1.jpg')" max-height="200" />
+        </v-col>
       </v-flex>
-      <v-flex xs12 md4>
-                <v-col xs12 md8>
-<v-card >
-  <v-col></v-col>
-  <h1>your reservation</h1>
-  <h2>kljvpsjvpsdjvpos</h2>
-  <h1>hiiiiiiiiiiiigrgrgergrfbfdbsbgf<br>brgreagreabreagaerrea</h1>
-</v-card>
-          </v-col>
+
+      <v-flex xs12 md8>
+        <v-col cols="auto" xs12 md8>
+          <v-card light>
+            <v-flex xs12>
+              <v-img
+                src="../assets/golflogo.jpg"
+                height="125px"
+                contain
+              ></v-img>
+           
+
+            <v-card-title class="headline"
+              >Green Valley Country Club Bangkok</v-card-title
+            >
+
+            <v-card-subtitle
+              >สนามกอล์ฟกรีนวัลเล่ คันทรีคลับ บางนา</v-card-subtitle
+            >
+             </v-flex>
+          </v-card>
+        </v-col>
       </v-flex>
+
+      <v-flex xs12 md8>
+        <v-col cols="auto" xs12 md8>
+          <v-card light>
+            <v-card-subtitle>search teetime</v-card-subtitle>
+            <v-progress-linear
+              color="red lighten-2"
+              buffer-value="0"
+              stream
+            ></v-progress-linear>
+            <v-card-subtitle>date</v-card-subtitle>
+          </v-card>
+        </v-col>
+      </v-flex>
+
+    
     </v-layout>
   </v-container>
 </template>
@@ -48,13 +54,7 @@ export default {
   name: "pichead",
 
   data: () => ({
-    items: [
-      {
-        color: "#ffffff",
-        title: "Green Valley Country Club Bangkok",
-        artist: "สนามกอล์ฟกรีนวัลเล่ คันทรีคลับ บางนา",
-      },
-    ],
+    //
   }),
 };
 </script>
