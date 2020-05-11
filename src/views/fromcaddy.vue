5e459f88f489a2b0b57f390debce095180fb3cea
janyamild <32623881+mildjny@users.noreply.github.com>
2020-05-11 21:03:28 +0700

    caddyinfo

--- src/views/fromcaddy.vue
+++ src/views/fromcaddy.vue
@@ -1,65 +1,115 @@
 <template>
-  <v-form v-model="valid">
-    <v-container>
-      <v-row>
-        <v-col
-          cols="6"
-          md="4"
+<v-container>
+<v-card  class="mx-auto"
+    max-width="500"
+    outlined col-md-6 offset-md-3>
+   <v-card-text>
+      <p class="display-1 text--primary">
+        caddy information
+      </p>
+   </v-card-text>
+  <v-col
+         
+          
         >
-          <v-text-field
-            v-model="firstname"
-            :rules="nameRules"
-            :counter="10"
-            label="First name"
-            required
-          ></v-text-field>
-        </v-col>
+  <v-form
+    ref="form"
+    v-model="valid"
+    lazy-validation
+  >
+  
+  <v-text-field
+      v-model="code"
+      :counter="3"
+      :rules="codeRules"
+      label="Code"
+      required
+    ></v-text-field>
 
-        <v-col
-          cols="6"
-          md="4"
-        >
-          <v-text-field
-            v-model="lastname"
-            :rules="nameRules"
-            :counter="10"
-            label="Last name"
-            required
-          ></v-text-field>
-        </v-col>
+  <v-text-field
+      v-model="fristname"
+      :rules="FnameRules"
+      label="Fristname"
+      required
+    ></v-text-field>
 
-        <v-col
-          cols="6"
-          md="4"
-        >
-          <v-text-field
-            v-model="email"
-            :rules="emailRules"
-            label="E-mail"
-            required
-          ></v-text-field>
-        </v-col>
-      </v-row>
-    </v-container>
+ <v-text-field
+      v-model="Lastname"
+      :rules="LnameRules"
+      label="Lastname"
+      required
+    ></v-text-field>
+
+     <v-text-field
+      v-model="nickname"
+      :rules="NInameRules"
+      label="nickname"
+      required
+    ></v-text-field>
+
+  <v-text-field
+      v-model="tel"
+      :counter="10"
+      :rules="telRules"
+      label="telephone"
+      required
+    ></v-text-field>
+    <v-file-input
+    label="File input"
+    filled
+    prepend-icon="mdi-camera"
+  ></v-file-input>
+    
   </v-form>
+   <div class="my-2">
+          <v-btn rounded block color="#05BC01" dark v-on:click="addinfo">confrim</v-btn>
+        </div>
+  </v-col>
+ 
+</v-card>
+</v-container>
+   
 </template>
 
 <script>
   export default {
-      name:'caddyinfo',
     data: () => ({
-      valid: false,
-      firstname: '',
-      lastname: '',
-      nameRules: [
-        v => !!v || 'Name is required',
-        v => v.length <= 10 || 'Name must be less than 10 characters',
+      valid: true,
+        code: '',
+      codeRules: [
+        v => !!v || 'code is required',
+        v => (v && v.length <= 3) || 'Name must be less than 3 nummeric',
       ],
-      email: '',
-      emailRules: [
-        v => !!v || 'E-mail is required',
-        v => /.+@.+/.test(v) || 'E-mail must be valid',
+      fristname: '',
+      FnameRules: [
+        v => !!v || 'Name is required'
       ],
+        Lastname: '',
+      LnameRules: [
+        v => !!v || 'Name is required'
+      ],
+        nickname: '',
+      NInameRules: [
+        v => !!v || 'Name is required'
+      ],
+         tel: '',
+      telRules: [
+        v => !!v || 'code is required',
+        v => (v && v.length <= 10) || 'Name must be less than 10 nummeric',
+      ],
+     
     }),
+
+    methods: {
+      validate () {
+        this.$refs.form.validate()
+      },
+      reset () {
+        this.$refs.form.reset()
+      },
+      resetValidation () {
+        this.$refs.form.resetValidation()
+      },
+    },
   }
 </script>
