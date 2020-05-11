5384d549f503225d76fc4c9cfcc6fdf62ab21ffc
janyamild <32623881+mildjny@users.noreply.github.com>
2020-05-11 19:58:59 +0700

    edit from

--- src/views/frominput.vue
+++ src/views/frominput.vue
@@ -2,28 +2,24 @@
  <v-container>
     <h2>test</h2>
     <div id="comment">
-<<<<<<< HEAD
     <v-col cols="12" sm="6"   v-for="(message, key) in comments"
           :key="key"
           >
-=======
-    <v-col cols="12" sm="6">
-      <div>
->>>>>>> ee6321f3df90fda3a3b9642603b027e4c6fe30bf
         <!--card-->
         <v-card max-width="344">
           <v-list-item three-line>
             <v-list-item-content>
-              <v-list-item-title class="headline mb-1">hello</v-list-item-title>
-              <v-list-item-subtitle>Pin</v-list-item-subtitle>
+              <v-list-item-title class="headline mb-1">{{message.comments}}</v-list-item-title>
+              <v-list-item-subtitle>{{message.name}}</v-list-item-subtitle>
             </v-list-item-content>
           </v-list-item>
         </v-card>
+        </v-col>
       </div>
 
       <br />
       <hr />
-    </v-col>
+    
     <!--form-->
     <v-col cols="12" sm="6">
       <div class="form-group">
