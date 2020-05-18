de50a683228d374b1106b4da132fc5c5067ee06a
janyamild <32623881+mildjny@users.noreply.github.com>
2020-05-18 15:16:20 +0700

    testloop

--- src/views/testloop.vue
+++ src/views/testloop.vue
@@ -6,19 +6,38 @@
           <v-card-subtitle>select time</v-card-subtitle>
           <v-divider></v-divider>
           <v-card-subtitle>teetime</v-card-subtitle>
-          <div v-for="(item, i) in items"
-            :key="i">
-          <v-col
-            class="text-center"
-            cols="12"
-            sm="4"
+        <v-row>
+        <v-col cols="12" sm="3">
            
-          >
-            <div class="my-2">
-              <v-btn small>{{ item.time }}</v-btn>
-            </div>
+              <v-btn small>06:00</v-btn>
+            
           </v-col>
-          </div>
+          <v-col cols="12" sm="3"
+           v-for="(item, i) in items"
+            :key="i">
+           
+              <v-btn small>{{ item.time }}</v-btn>
+            
+          </v-col>
+
+           <v-col cols="12" sm="3">
+            <v-btn icon color="pink">
+              <v-icon>mdi-heart</v-icon>
+            </v-btn>
+          </v-col>
+
+          <v-col cols="12" sm="3">
+            <v-btn icon color="indigo">
+              <v-icon>mdi-star</v-icon>
+            </v-btn>
+          </v-col>
+
+          <v-col cols="12" sm="3">
+            <v-btn icon color="green">
+              <v-icon>mdi-cached</v-icon>
+            </v-btn>
+          </v-col>
+        </v-row>
         </v-card>
       </v-col>
     </v-flex>
