37d59ad6daa18dad647a86e15bdc7d1387111453
janyamild <32623881+mildjny@users.noreply.github.com>
2020-06-15 14:18:28 +0700

    Drawer nav

--- src/components/navigation.vue
+++ src/components/navigation.vue
@@ -12,7 +12,7 @@
       />
     </div>
    <v-spacer></v-spacer>
-    <v-toolbar-items center>
+    <!-- <v-toolbar-items center>
          <v-btn
          color="#0F7D63"
            v-for="item in nav"
@@ -22,7 +22,7 @@
            flat
          >{{ item.text }}</v-btn>
         </v-toolbar-items>
-      
+       -->
 
     </v-app-bar>
 
@@ -35,32 +35,32 @@
        name: "navigation",
     
       data: () => ({
-     nav: [
-        {
-          icon: 'home',
-          text: 'Teetime',
-          title: 'Back to Booling page',
-          active: true
-        },
-        {
-          icon: 'info',
-          text: 'Caddy',
-          title: 'About Caddy',
-          active: false
-        },
-        {
-          icon: 'assignment_turned_in',
-          text: 'Member',
-          title: 'member',
-          active: false
-        },
-        {
-          icon: 'email',
-          text: 'Starter',
-          title: 'starter',
-          active: false
-        }
-      ]
+    //  nav: [
+    //     {
+    //       icon: 'home',
+    //       text: 'Teetime',
+    //       title: 'Back to Booling page',
+    //       active: true
+    //     },
+    //     {
+    //       icon: 'info',
+    //       text: 'Caddy',
+    //       title: 'About Caddy',
+    //       active: false
+    //     },
+    //     {
+    //       icon: 'assignment_turned_in',
+    //       text: 'Member',
+    //       title: 'member',
+    //       active: false
+    //     },
+    //     {
+    //       icon: 'email',
+    //       text: 'Starter',
+    //       title: 'starter',
+    //       active: false
+    //     }
+    //   ]
   })
 };
 </script>
