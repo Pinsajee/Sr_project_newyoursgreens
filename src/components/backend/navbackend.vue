d37a983d1997a74e54c1bd756ce635270b93ead1
janyamild <32623881+mildjny@users.noreply.github.com>
2020-06-16 14:56:12 +0700

    nav edit

--- src/components/backend/navbackend.vue
+++ src/components/backend/navbackend.vue
@@ -1,38 +1,53 @@
 <template>
-
-  <v-app-bar app color="#0F7D63" dark>
-    <div class="d-flex align-center">
-      <v-img
-        :src="require('../assets/logow.png')"
-        alt="Logo"
-        class="shrink mr-2"
-        contain
-        transition="scale-transition"
-        width="50"
-      />
-    </div>
-    <v-spacer></v-spacer>
-   
-    <v-btn></v-btn>
-    <v-btn></v-btn>
-    <v-btn></v-btn>
+  <v-navigation-drawer id="app-drawer" class=" lighten-1"  permanent>
+    <v-list>
+            <v-list-tile avatar>
+              <v-list-tile-avatar color="white">
+                <v-img  
+                :src="require('../../assets/golflogo.jpg')"
+                 height="70" 
+                 contain 
+                  class=" darken-5"></v-img>
+                  <br>
+                  <h4  >Green Valley Country Club Bangkok</h4>
+                  <br>
+              </v-list-tile-avatar>
+            </v-list-tile>
+    
       
-    </v-app-bar>
+      <v-list-item v-for="item in items" :key="item.title" link>
+        <v-list-item-icon>
+          <v-icon>{{ item.icon }}</v-icon>
+        </v-list-item-icon>
+
+        <v-list-item-content>
+          <v-list-item-title>{{ item.title }}</v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+    </v-list>
+
+    <!-- <template v-slot:append>
+      <div class="pa-2">
+        <v-btn block>Logout</v-btn>
+      </div>
+    </template> -->
+  </v-navigation-drawer>
+
 
 </template>
 
-
-
 <script>
-  export default {
-       name: "navigation",
-    
-      data: () => ({
-    //
-  })
+export default {
+  data() {
+    return {
+      items: [
+        { title: "Teetime", icon: "dashboard" },
+        { title: "Caddy", icon: "account_box" },
+        { title: "Member", icon: "gavel" },
+      ],
+    };
+  },
 };
 </script>
 
-<style >
-
-</style>
+<style></style>
